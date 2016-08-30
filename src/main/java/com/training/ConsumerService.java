41b2c18375084a260a29f5d7db6aea680ac3e8ac
SunilKumarPeljur <sunilkumar92097@gmail.com>
2016-08-30 18:01:31 +0530

    Next commit

--- src/main/java/com/training/ConsumerService.java
+++ src/main/java/com/training/ConsumerService.java
@@ -4,12 +4,19 @@
 package com.training;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +26,7 @@ import org.springframework.web.client.RestTemplate;
  */
 
 @RestController
+@RequestMapping("/api")
 public class ConsumerService {
 
 	@Autowired
@@ -28,13 +36,28 @@ public class ConsumerService {
 	public List<Orders> getData() {
 		
 		List<ServiceInstance> list = client.getInstances("orders-ms");
+		/*Map<String,Object> map = new HashMap<String, Object>();
+		map.put("id", id);*/
+		if(list != null && !list.isEmpty()) {
+			URI uri = list.get(0).getUri();
+			if(uri != null) {
+				return (new RestTemplate()).getForObject(uri.toString() + "/api/order", List.class); 
+			}
+		}
+		return null;
+	}
+	
+	@RequestMapping(value="/orderPost", method=RequestMethod.POST)
+	public ResponseEntity<String> postOrders(@RequestBody Orders orders) {
+		
+		List<ServiceInstance> list = client.getInstances("orders-ms");
 		
 		if(list != null && !list.isEmpty()) {
 			URI uri = list.get(0).getUri();
 			if(uri != null) {
-				return (new RestTemplate()).getForObject(uri, List.class);	 
+				return (new RestTemplate()).postForEntity(uri + "/api/order", orders, String.class);
 			}
 		}
-		return null;
+		return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
 	}
 }
